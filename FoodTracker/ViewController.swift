407e9a4055ef8de91d23f512ecd79e3d4f2d3245
Aly <a.micki@lescomptoirs.com>
2017-04-20 10:57:06 +0200

    Initial commit

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var enteryourMealTextField: UITextField!
+    @IBOutlet weak var yourMealLabel: UILabel!
+    @IBOutlet weak var imageSelector: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        imageSelector.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Actions    
+    @IBAction func imageSelectorTapped(_ sender: UITapGestureRecognizer) {
+        enteryourMealTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        //imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        yourMealLabel.text = enteryourMealTextField.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        enteryourMealTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
